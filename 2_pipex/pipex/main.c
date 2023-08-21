ee2408071e961fe130459e155cce4b5e4e4679ee
Seungwon An <seunan@cx2r8s1.42seoul.kr>
2023-08-22 00:23:28 +0900

    norm

--- 2_pipex/pipex/main.c
+++ 2_pipex/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: seunan <seunan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 16:47:33 by seunan            #+#    #+#             */
-/*   Updated: 2023/08/22 00:20:06 by seunan           ###   ########.fr       */
+/*   Updated: 2023/08/22 00:21:34 by seunan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,18 @@ int	main(int ac, char *av[], char *envp[])
 
 	if (ac != 5)
 		exit_with_msg("Error: arguments\n");
-	path = parse_path(envp); 				// PATH 환경변수를 파싱하여 저장
-	if (pipe(fd) == -1) 					// 파이프 생성
+	path = parse_path(envp);
+	if (pipe(fd) == -1)
 		exit_with_msg("Error: pipe\n");
-
 	pid = fork();
-	if (pid == 0) 							// children infile -> cmd1 -> fd[1]
+	if (pid == 0)
 		infile_to_fd(av, path, fd);
-	else // parent process
+	else
 	{
 		pid = fork();
-		if (pid == 0) 						// children fd[0] -> cmd2 -> outfile
+		if (pid == 0)
 			fd_to_outfile(av, path, fd);
 		wait(NULL);
 	}
 	return (0);
 }
-
-
--- 2_pipex/pipex/utils.c
+++ 2_pipex/pipex/utils.c
@@ -6,7 +6,7 @@
 /*   By: seunan <seunan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:36:27 by seunan            #+#    #+#             */
-/*   Updated: 2023/08/22 00:19:37 by seunan           ###   ########.fr       */
+/*   Updated: 2023/08/22 00:23:06 by seunan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ void	fd_to_outfile(char *av[], char *path[], int fd[2])
 	char	**execve_argv;
 	char	*execve_path;
 
-	outfile = protected_open(av[4], O_WRONLY | O_CREAT | O_TRUNC); 	// 출력 파일을 쓰기, 없으면 생성 후 열기
-	protected_close(fd[WRITE_END]); 								// 자식 프로세스는 파이프에서 쓰지 않으므로 쓰는 쪽을 닫는다.
-	protected_dup2(fd[READ_END], STDIN_FILENO); 					// 파이프의 읽기 쪽을 표준 입력으로 복제
-	protected_dup2(outfile, STDOUT_FILENO); 						// 출력 파일을 표준 출력으로 복제
+	outfile = protected_open(av[4], O_WRONLY | O_CREAT | O_TRUNC);
+	protected_close(fd[WRITE_END]);
+	protected_dup2(fd[READ_END], STDIN_FILENO);
+	protected_dup2(outfile, STDOUT_FILENO);
 	execve_argv = ft_split(av[3], ' ', '\0');
-	execve_path = ft_strjoin(path[valid_path(path, execve_argv[0])], execve_argv[0]);
+	execve_path = ft_strjoin(path[valid_path(path, execve_argv[0])],
+			execve_argv[0]);
 	execve(execve_path, execve_argv, NULL);
 	exit_with_msg("Error: execve\n");
 }
@@ -40,17 +41,18 @@ void	infile_to_fd(char *av[], char *path[], int fd[2])
 	char	**execve_argv;
 	char	*execve_path;
 
-	infile = protected_open(av[1], O_RDONLY); 		// 입력 파일을 읽기 전용으로 열기
-	protected_close(fd[READ_END]); 					// 자식 프로세스는 파이프에서 읽지 않으므로 읽는 쪽을 닫는다.
-	protected_dup2(infile, STDIN_FILENO); 			// infile을 표준 입력으로 복제
-	protected_dup2(fd[WRITE_END], STDOUT_FILENO); 	// 파이프의 쓰기 쪽을 표준 출력으로 복제
+	infile = protected_open(av[1], O_RDONLY);
+	protected_close(fd[READ_END]);
+	protected_dup2(infile, STDIN_FILENO);
+	protected_dup2(fd[WRITE_END], STDOUT_FILENO);
 	execve_argv = ft_split(av[2], ' ', '\0');
-	execve_path = ft_strjoin(path[valid_path(path, execve_argv[0])], execve_argv[0]);
+	execve_path = ft_strjoin(path[valid_path(path, execve_argv[0])],
+			execve_argv[0]);
 	execve(execve_path, execve_argv, NULL);
 	exit_with_msg("Error: execve\n");
 }
 
-int		valid_path(char *path[], char *cmd)
+int	valid_path(char *path[], char *cmd)
 {
 	int	i;
 
@@ -67,10 +69,11 @@ int		valid_path(char *path[], char *cmd)
 
 char	**parse_path(char *envp[])
 {
-	char	**path = NULL;
+	char	**path;
 	char	*tmp;
 	int		i;
 
+	path = NULL;
 	i = 0;
 	while (envp[i] != NULL)
 	{
