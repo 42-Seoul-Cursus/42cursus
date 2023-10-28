d80c2b0603bae198bff5b6bdd61487a6c996f4f0
seungwonme <seungwonan.me@gmail.com>
2023-10-29 01:31:03 +0900

    cpp 00 ex00

--- 4_cpp_module/cpp_module_00/ex00/megaphone.cpp
+++ 4_cpp_module/cpp_module_00/ex00/megaphone.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-int	main(int ac, char *av[])
+int main(int ac, char const *av[])
 {
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
-		std::cout << av[i];
+	{
+		for (int j = 0; av[i][j] != '\0'; j++)
+		{
+			if ('a' <= av[i][j] && av[i][j] <= 'z')
+				std::cout << (char)(av[i][j] - 32);
+			else
+				std::cout << (char)av[i][j];
+		}
+	}
 	std::cout << std::endl;
-	return (0);
+	return 0;
 }
