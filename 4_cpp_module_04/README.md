8c0e3525ebebb9ba80a3407d42ee44235e7e72f3
seungwonme <seungwonan.me@gmail.com>
2023-11-28 15:10:11 +0900

    cpp01

--- 4_cpp_module_04/README.md
+++ 4_cpp_module_04/README.md
@@ -293,3 +293,21 @@
 
 - class1 내부의 크기가 8인  class2 배열은 class1 생성 시 class2 생성자 호출이 8번 일어난다.
 - cpp의 파일 입출력은 [fstream](https://cplusplus.com/reference/fstream/fstream/)를 사용한다.
+  - [참고 블로그](https://blockdmask.tistory.com/322)
+	```cpp
+	// 원래 짰던 코드
+	// while (!readFile.eof())
+	// {
+	// 	std::string s;
+
+	// 	std::getline(readFile, s);
+	// 	// s.append("\n");
+	// 	writeFile.write(s.c_str(), s.length());
+	// }
+
+	char c;
+  while (readFile.get(c))  // get 함수로 문자 하나씩 읽어오기
+  {
+      writeFile.put(c);  // 파일에 문자 하나씩 쓰기
+  }
+	```
--- 4_cpp_module_04/cpp_module_01/ex04/main.cpp
+++ 4_cpp_module_04/cpp_module_01/ex04/main.cpp
@@ -10,21 +10,42 @@ int main(int ac, char const *av[])
 		return 1;
 	}
 
-	std::ifstream ifs(av[1]);
-	if (!ifs)
+	std::ifstream readFile(av[1]);
+	if (!readFile)
 	{
 		std::cerr << "Error!" << std::endl;
 		return 1;
 	}
 
-	std::string file;
-	ifs >> file;
-	for (int i = 0; i < 100; i++)
-	{
-		std::cout << file << '\n';
-		ifs >> file;
-	}
+	std::string fileName = av[1];
+	fileName.append(".replace");
 
+	std::ofstream writeFile(fileName);
+
+	while (!readFile.eof())
+	{
+		std::string line;
+
+		std::getline(readFile, line);
+
+		/* line에서 문자열 s1을 s2로 바꿔주는 작업 */
+		while (line.find(av[2]) != std::string::npos)
+		{
+			int len = 0;
+			while (av[2][len] != '\0')
+			{
+				++len;
+			}
+			line.replace(line.find(av[2]), len, av[3]);
+		}
+		/* --------------------------------------- */
+
+		if (!readFile.eof())
+		{
+			line.append("\n");
+		}
+		writeFile.write(line.c_str(), line.length());
+	}
 
 	return 0;
 }
