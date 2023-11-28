46687da690cf33306dc45d182ce767837e338fa8
seungwonme <seungwonan.me@gmail.com>
2023-11-28 21:33:36 +0900

    cpp01

--- 4_cpp_module_04/cpp_module_01/ex06/Karen.cpp
+++ 4_cpp_module_04/cpp_module_01/ex06/Karen.cpp
@@ -28,17 +28,17 @@ void Karen::complain(std::string level)
 }
 void Karen::debug( void )
 {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n\n";
 }
 void Karen::info( void )
 {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don\'t put enough! If you did I would not have to ask for it!\n";
+	std::cout << "I cannot believe adding extra bacon cost more money. You don\'t put enough! If you did I would not have to ask for it!\n\n";
 }
 void Karen::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I\'ve been coming here for years and you just started working here last month.\n";
+	std::cout << "I think I deserve to have some extra bacon for free.\nI\'ve been coming here for years and you just started working here last month.\n\n";
 }
 void Karen::error( void )
 {
-	std::cout <<  "This is unacceptable, I want to speak to the manager now.\n";
+	std::cout <<  "This is unacceptable, I want to speak to the manager now.\n\n";
 }
