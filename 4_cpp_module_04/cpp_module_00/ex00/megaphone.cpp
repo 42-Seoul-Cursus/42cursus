e77ab73b1b5589e22f1d67cc21ca7426f398e629
seunan <seunan@student.42seoul.kr>
2024-01-03 19:40:03 +0900

    Use cpp style in ex00

--- 4_cpp_module_04/cpp_module_00/ex00/megaphone.cpp
+++ 4_cpp_module_04/cpp_module_00/ex00/megaphone.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 
-static void PrintLowerToUpper(const char c)
-{
-	if ('a' <= c && c <= 'z')
-	{
-		std::cout << (char)(c - 32);
-	}
-	else
-	{
-		std::cout << (char)c;
-	}
-}
-
 int main(int ac, char const *av[])
 {
+	std::string s;
+
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	}
 	for (int i = 1; i < ac; i++)
 	{
-		for (int j = 0; av[i][j] != '\0'; j++)
+		s = av[i];
+		for (size_t j = 0; j < s.length(); j++)
 		{
-			PrintLowerToUpper(av[i][j]);
+			std::cout << static_cast<char>(toupper(s[j]));
 		}
 	}
 	std::cout << std::endl;
