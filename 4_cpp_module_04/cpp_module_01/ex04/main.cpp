423a3de2208e9723e13eb295c47b1a979d4c21a5
seunan <seunan@student.42seoul.kr>
2024-01-05 01:08:32 +0900

    Finish ex04

--- 4_cpp_module_04/cpp_module_01/ex04/main.cpp
+++ 4_cpp_module_04/cpp_module_01/ex04/main.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <string>
 
+#include <unistd.h>
 int main(int ac, char const *av[])
 {
 	if (ac != 4)
 	{
-		std::cout << "Usage: ./replace filename s1 s2\n";
+		std::cout << "\033[37mUsage: ./replace filename s1 s2\n";
 		return 1;
 	}
 
@@ -14,7 +15,7 @@ int main(int ac, char const *av[])
 	std::ifstream readFile(av[1]);
 	if (!readFile)
 	{
-		std::cerr << "\033[31mFile open error" << std::endl;
+		std::cerr << "\033[31mError: Unable to open input file \'" << av[1] << '\'' << std::endl;
 		return 1;
 	}
 
@@ -23,6 +24,11 @@ int main(int ac, char const *av[])
 
 	// 파일 쓰기
 	std::ofstream writeFile(fileName);
+	if (!writeFile)
+	{
+		std::cerr << "\033[31mError: Unable to open output file \'" << fileName << '\'' << std::endl;
+		return 1;
+	}
 
 	int beforeLen = 0;
 
@@ -39,18 +45,21 @@ int main(int ac, char const *av[])
 		std::getline(readFile, line);
 		size_t idx = line.find(av[2]);
 
-		if (idx != std::string::npos)
+		while (idx != std::string::npos)
 		{
 			replace += line.substr(0, idx);
 			replace += av[3];
 			replace += line.substr(idx + beforeLen);
 			line = replace;
+			idx = line.find(av[2]);
+			replace = "";
 		}
 
 		if (!readFile.eof())
 		{
 			line.append("\n");
 		}
+
 		writeFile.write(line.c_str(), line.length());
 	}
 
