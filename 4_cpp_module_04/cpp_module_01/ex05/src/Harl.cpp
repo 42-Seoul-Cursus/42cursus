dbdc72e66b0fbee758dd68562dec324e2c332949
seunan <seunan@student.42seoul.kr>
2024-01-05 15:35:56 +0900

    Change endline charator to std::endl

--- 4_cpp_module_04/cpp_module_01/ex05/src/Harl.cpp
+++ 4_cpp_module_04/cpp_module_01/ex05/src/Harl.cpp
@@ -20,17 +20,20 @@ void Harl::complain(std::string level)
 }
 void Harl::debug( void )
 {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I just love it!" << std::endl;
 }
 void Harl::info( void )
 {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don\'t put enough! If you did I would not have to ask for it!\n";
+	std::cout << "I cannot believe adding extra bacon cost more money." << std::endl;
+	std::cout << "You don\'t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 void Harl::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I\'ve been coming here for years and you just started working here last month.\n";
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I\'ve been coming here for years and you just started working here last month." << std::endl;
 }
 void Harl::error( void )
 {
-	std::cout <<  "This is unacceptable, I want to speak to the manager now.\n";
+	std::cout <<  "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
