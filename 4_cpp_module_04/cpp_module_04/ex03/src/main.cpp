fa7ecbe6d7ee1821347431281b80726013aa39e9
seungwonme <seungwonan.me@gmail.com>
2024-01-20 22:30:19 +0900

    Modify main.c

--- 4_cpp_module_04/cpp_module_04/ex03/src/main.cpp
+++ 4_cpp_module_04/cpp_module_04/ex03/src/main.cpp
@@ -4,9 +4,6 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
-// export MallocStackLogging=1 find leaks after unset MallocStackLogging
-// if you want more info about leaks use gcc -g option
-// while true; do leaks a.out; sleep 1; done;
 void	leak(void)
 {
 	system("leaks --list interface");
