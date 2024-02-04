c5bc357abc36778d89d49460eeb5a6cc9f76c809
seungwonme <seungwonan.me@gmail.com>
2024-02-04 23:48:12 +0900

    Solving ex02

--- 5_cpp_module_09/cpp_module_08/ex02/inc/MutantStack.hpp
+++ 5_cpp_module_09/cpp_module_08/ex02/inc/MutantStack.hpp
@@ -1,15 +1,25 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <iosfwd>
+#include <stack>
+#include <iterator>
 
-class MutantStack
+template <typename T>
+class MutantStack : public std::stack<T, std::deque<T> >
 {
-private:
-	/* data */
 public:
-	MutantStack(/* args */);
-	~MutantStack();
+	class iterator : public std::deque<T>::iterator 
+	{
+		iterator(typename std::deque<T>::iterator it) : std::deque<T>::iterator(it) {}
+	};
+	iterator begin() 
+	{
+        return c.begin();
+    }
+    iterator end() 
+	{
+        return c.end();
+    }
 };
 
 #include "MutantStack.tpp"
