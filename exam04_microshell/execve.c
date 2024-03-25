b6d412e3380fd12e714b764e57cf8577f3c1f2be
seungwonme <seungwonan.me@gmail.com>
2024-03-26 03:39:49 +0900

    Committing to run a testcase in a GitHub code space
    - Running locally turns off the computer
    - Turned off twice and went to purple screen

--- exam04_microshell/execve.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    execve("/bin/ls", (char *[]){"ls", "-a", NULL}, NULL);
-    printf("FAIL\n");
-    return 0;
-}
--- exam04_microshell/microshell.c
+++ exam04_microshell/microshell.c
@@ -3,17 +3,24 @@
 #include <string.h>
 #include <unistd.h>
 
-const int   READ_END = 0;
-const int   WRITE_END = 1;
+const int READ_END = 0;
+const int WRITE_END = 1;
 
-int         g_ac;
-char**      g_av;
-int         g_av_idx = 1;
-int         g_process_cnt = 0;
-int         g_fd[2];
-int         g_prev_fd;
+int     g_ac;
+char    **g_av;
+int     g_av_idx = 1;
+int     g_process_cnt = 0;
+int     g_fd[2];
+int     g_prev_fd;
 
 // ["a.out", "/bin/ls", "|", "/bin/cat", "|", "/bin/cat", "|", "/bin/cat"]
+
+void cd()
+{
+    chdir(g_av[++g_av_idx]);
+    system("pwd");
+    printf("%s\n", g_av[g_av_idx]);
+}
 
 int get_pipe_cnt()
 {
@@ -25,17 +32,35 @@ int get_pipe_cnt()
         {
             ++cnt;
         }
+        if (strncmp(g_av[i], ";", 2) == 0)
+        {
+            return (cnt);
+        }
     }
     return (cnt);
 }
 
-void    middle()
+int get_command_cnt()
+{
+    int cnt = 1;
+
+    for (int i = 1; i < g_ac; i++)
+    {
+        if (strncmp(g_av[i], ";", 2) == 0)
+        {
+            ++cnt;
+        }
+    }
+    return (cnt);
+}
+
+void middle()
 {
     int start = g_av_idx;
     g_prev_fd = g_fd[READ_END];
     pipe(g_fd);
 
-    while (strncmp(g_av[g_av_idx], "|", 2) != 0)
+    while (g_av[g_av_idx] && strncmp(g_av[g_av_idx], "|", 2) != 0)
     {
         ++g_av_idx;
     }
@@ -57,7 +82,7 @@ void    middle()
     }
 }
 
-void    last()
+void last()
 {
     int start = g_av_idx;
     pid_t pid;
@@ -69,18 +94,18 @@ void    last()
         dup2(g_fd[READ_END], STDIN_FILENO);
         execve(g_av[g_av_idx], g_av + start, NULL);
     }
-    else 
+    else
     {
         close(g_fd[READ_END]);
     }
 }
 
-void    first()
+void first()
 {
     int start = g_av_idx;
     pid_t pid;
 
-    while (strncmp(g_av[g_av_idx], "|", 2) != 0)
+    while (g_av[g_av_idx] && strncmp(g_av[g_av_idx], "|", 2) != 0)
     {
         ++g_av_idx;
     }
@@ -99,36 +124,85 @@ void    first()
         execve(g_av[g_av_idx - 1], g_av + start, NULL);
         printf("FAIL\n");
     }
-    else 
+    else
     {
         close(g_fd[WRITE_END]);
         ++g_av_idx;
     }
 }
 
-int	main(int ac, char *av[])
+void run_command()
 {
-    g_ac = ac; g_av = av;
     int pipe_cnt = get_pipe_cnt();
-
-    for (; g_process_cnt <= pipe_cnt; g_process_cnt++)
+    if (pipe_cnt == 0)
     {
-        if (g_process_cnt == 0)
+        if (strncmp(g_av[g_av_idx], "cd", 3) == 0)
         {
-            first();
-        }
-        else if (g_process_cnt == pipe_cnt)
-        {
-            last();
+            cd();
         }
         else
         {
-            middle();
+            int start = g_av_idx;
+            pid_t pid;
+
+            while (g_av[g_av_idx] && strncmp(g_av[g_av_idx], "|", 2) != 0)
+            {
+                ++g_av_idx;
+            }
+            pid = fork();
+
+            // 자식
+            if (pid == 0)
+            {
+                close(g_fd[READ_END]);
+                dup2(g_fd[WRITE_END], STDOUT_FILENO);
+                g_av[g_av_idx] = NULL;
+                execve(g_av[g_av_idx - 1], g_av + start, NULL);
+            }
+            ++g_av_idx;
+        }
+    }
+    else
+    {
+        for (; g_process_cnt <= pipe_cnt; g_process_cnt++)
+        {
+            if (g_process_cnt == 0)
+            {
+                first();
+            }
+            else if (g_process_cnt == pipe_cnt)
+            {
+                last();
+            }
+            else
+            {
+                middle();
+            }
         }
     }
     for (int i = 0; i <= pipe_cnt; i++)
     {
         wait(NULL);
+    }
+}
+
+int main(int ac, char *av[])
+{
+    g_ac = ac;
+    g_av = av;
+    if (g_ac == 1)
+    {
+        return (1);
+    }
+    int command_cnt = get_command_cnt();
+    for (int i = 0; i < command_cnt; i++)
+    {
+        run_command();
+        while (g_av[g_av_idx] && strncmp(g_av[g_av_idx], ";", 2) != 0)
+        {
+            ++g_av_idx;
+        }
+        ++g_av_idx;
     }
     return (0);
 }
