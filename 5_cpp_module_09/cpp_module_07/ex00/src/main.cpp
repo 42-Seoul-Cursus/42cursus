3da7cfd17c65b9eca45e611e0c724b398c109ec0
seungwonme <seungwonan.me@gmail.com>
2024-04-18 23:12:10 +0900

    Update

--- 5_cpp_module_09/cpp_module_07/ex00/src/main.cpp
+++ 5_cpp_module_09/cpp_module_07/ex00/src/main.cpp
@@ -8,33 +8,49 @@ int	main()
 	double aDouble = 1.1, bDouble = 2.2;
 	char aChar = 'a', bChar = 'b';
 
-	::swap(aInt, bInt);
 	std::cout << "\033[0;31m" << "INT TEST" << '\n'
-	<< "aInt: " << aInt << ' ' << &aInt << '\n'
-	<< "bInt: " << bInt << ' ' << &bInt << '\n'
+    << "Before swap(aInt, bInt)" << "\n\n"
+    << "aInt: " << aInt << ' ' << &aInt << '\n'
+	<< "bInt: " << bInt << ' ' << &bInt << "\n\n";
+	::swap(aInt, bInt);
+	std::cout << "After swap(aInt, bInt)" << "\n\n"
+    << "aInt: " << aInt << ' ' << &aInt << '\n'
+	<< "bInt: " << bInt << ' ' << &bInt << "\n\n"
 	<< "max: " << ::max(aInt, bInt) << '\n'
-	<< "max: " << ::min(aInt, bInt) << '\n' << '\n';
+	<< "min: " << ::min(aInt, bInt) << "\n\n";
 
-	::swap(aFloat, bFloat);
 	std::cout << "\033[0;33m" << "FLOAT TEST" << '\n'
+    << "Before swap(aFloat, bFloat)" << "\n\n"
+    << "aFloat: " << aFloat << ' ' << &aFloat << '\n'
+	<< "bFloat: " << bFloat << ' ' << &bFloat << "\n\n";
+	::swap(aFloat, bFloat);
+    std::cout << "After swap(aFloat, bFloat)" << "\n\n"
 	<< "aFloat: " << aFloat << ' ' << &aFloat << '\n'
-	<< "bFloat: " << bFloat << ' ' << &bFloat << '\n'
+	<< "bFloat: " << bFloat << ' ' << &bFloat << "\n\n"
 	<< "max: " << ::max(aFloat, bFloat) << '\n'
-	<< "max: " << ::min(aFloat, bFloat) << '\n' << '\n';
+	<< "min: " << ::min(aFloat, bFloat) << "\n\n";
 
-	::swap(aDouble, bDouble);
 	std::cout << "\033[0;32m" << "DOUBLE TEST" << '\n'
+    << "Before swap(aDouble, bDouble)" << "\n\n"
+    << "aDouble: " << aDouble << ' ' << &aDouble << '\n'
+	<< "bDouble: " << bDouble << ' ' << &bDouble << "\n\n";
+	::swap(aDouble, bDouble);
+    std::cout << "After swap(aDouble, bDouble)" << "\n\n"
 	<< "aDouble: " << aDouble << ' ' << &aDouble << '\n'
-	<< "bDouble: " << bDouble << ' ' << &bDouble << '\n'
+	<< "bDouble: " << bDouble << ' ' << &bDouble << "\n\n"
 	<< "max: " << ::max(aDouble, bDouble) << '\n'
-	<< "max: " << ::min(aDouble, bDouble) << '\n' << '\n';
+	<< "min: " << ::min(aDouble, bDouble) << "\n\n";
 
+	std::cout << "\033[0;34m" << "CHARACTER TEST" << '\n'
+    << "Before swap(aChar, bChar)" << "\n\n"
+    << "aChar: " << aChar << ' ' << static_cast<void*>(&aChar) << '\n'
+	<< "bChar: " << bChar << ' ' << static_cast<void*>(&bChar) << "\n\n";
 	::swap(aChar, bChar);
-	std::cout << "\033[0;34m" << "CHAR TEST" << '\n'
+    std::cout << "After swap(aChar, bChar)" << "\n\n"
 	<< "aChar: " << aChar << ' ' << static_cast<void*>(&aChar) << '\n'
-	<< "bChar: " << bChar << ' ' << static_cast<void*>(&bChar) << '\n'
+	<< "bChar: " << bChar << ' ' << static_cast<void*>(&bChar) << "\n\n"
 	<< "max: " << ::max(aChar, bChar) << '\n'
-	<< "max: " << ::min(aChar, bChar) << "\033[0m" << std::endl;;
+	<< "min: " << ::min(aChar, bChar) << std::endl;
 
 	return 0;
 }
