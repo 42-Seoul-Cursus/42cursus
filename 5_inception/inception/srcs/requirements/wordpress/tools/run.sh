9dc42adf289ca857652b7d4ace681ed6e4278f0f
seungwonme <seungwonan.me@gmail.com>
2024-05-30 18:33:24 +0900

    Modify script using wp-cli

--- 5_inception/inception/srcs/requirements/wordpress/tools/run.sh
+++ 5_inception/inception/srcs/requirements/wordpress/tools/run.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-while true; do
+while [ ! -e /var/www/html/wp-config.php ]; do
     # https://make.wordpress.org/cli/handbook/guides/quick-start/
     /scripts/wp-cli.phar core config --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --dbprefix=${WORDPRESS_TABLE_PREFIX} --path=/var/www/html --allow-root
-    if [ $? -eq 0 ]; then
-        break
-    fi
 done
 
 /scripts/wp-cli.phar core install --url=${DOMAIN_NAME} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN_ID} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --path=/var/www/html --allow-root
